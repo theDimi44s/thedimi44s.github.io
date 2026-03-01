8e7d6a317d2dafacbd47cdafe7e6dd01c3e7d0bf
Dmytro Yevtushenko <dmitriyyevtushenko@icloud.com>
2026-03-01 17:04:28 +0200

    Fix empty page

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        puzzle: resolve(__dirname, 'puzzle.html') // Вказуємо нашу нову сторінку
       }
     }
   }
